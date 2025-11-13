2aaf1d693c9e3b46ed065b9803eeaa075a53d463
Agata Guć <agata.halas.dev@gmail.com>
2025-11-13 11:16:43 +0100

    Release v1.1.2
    Bump version to 1.1.2 in:
    - frontend/package.json
    - frontend/src/version.ts

--- frontend/src/version.ts
+++ frontend/src/version.ts
@@ -1,4 +1,4 @@
 // Application version
 // This file is automatically updated by release.sh
-export const APP_VERSION = '1.1.1';
+export const APP_VERSION = '1.1.2';
 export const APP_NAME = 'OnionTravel';
