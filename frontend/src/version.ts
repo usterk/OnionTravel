20243bb9f90dbb350f90e0e7bb3b29f6e619a835
Paweł Guć <usterk@gmail.com>
2025-11-16 09:43:19 +0700

    Release v1.4.0
    Bump version to 1.4.0 in:
    - frontend/package.json
    - frontend/src/version.ts

--- frontend/src/version.ts
+++ frontend/src/version.ts
@@ -1,4 +1,4 @@
 // Application version
 // This file is automatically updated by deploy-prod.sh
-export const APP_VERSION = '1.3.0';
+export const APP_VERSION = '1.4.0';
 export const APP_NAME = 'OnionTravel';
